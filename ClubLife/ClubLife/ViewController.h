07f98dc68dfaf24b3aeaad8fa53952790f9089c1
MartinLi841538513 <841538513@qq.com>
2014-12-18 14:49:10 +0800

    first commit

---
+++ ClubLife/ClubLife/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ClubLife
+//
+//  Created by Gao Huang on 14-12-18.
+//  Copyright (c) 2014年 Martin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ClubLife/ClubLifeTests/ClubLifeTests.m
@@ -0,0 +1,40 @@
+//
+//  ClubLifeTests.m
+//  ClubLifeTests
+//
+//  Created by Gao Huang on 14-12-18.
+//  Copyright (c) 2014年 Martin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface ClubLifeTests : XCTestCase
+
+@end
+
+@implementation ClubLifeTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
